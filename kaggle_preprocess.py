b9e575ad2fb2823c1e4fcca4cb426f62a2931eaa
lucemia <lucemia@gmail.com>
2014-08-04 04:54:40 +0000

    add scale in preprocess stage, fix bug

--- kaggle_preprocess.py
+++ kaggle_preprocess.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from sklearn.preprocessing import Imputer
+from sklearn import preprocessing
 import fileinput
+import numpy
 
 def imputer():
     train = []
@@ -12,10 +14,16 @@ def imputer():
             pass
 
     imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+#    print train
     imp.fit(train)
+#    print train
     train = imp.transform(train)
+    train = preprocessing.scale(train)
+
+#    print train
+
     for iline in train:
-        print list(iline)
+        print ",".join(map(str, iline))
 
 if __name__ == "__main__":
     imputer()
