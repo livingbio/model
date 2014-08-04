112fded13bceb564a87ef118c494122b94d18654
lucemia <lucemia@gmail.com>
2014-08-04 12:24:54 +0800

    work on imputer preprocess

---
+++ kaggle_preprocess.py
@@ -0,0 +1,20 @@
+from sklearn.preprocessing import Imputer
+import fileinput
+
+def imputer():
+    train = []
+    for iline in fileinput.input():
+        iline = iline.strip().split(',')
+        try:
+            train.append([float(x) if x else numpy.NaN for x in iline])
+        except:
+            pass
+
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imp.fit(train)
+    train = imp.transform(train)
+    for iline in train:
+        print list(iline)
+
+if __name__ == "__main__":
+    imputer()
