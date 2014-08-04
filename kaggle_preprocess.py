e0f35a3835dbf450a3721b737975a3ffed8b89ad
lucemia <lucemia@gmail.com>
2014-08-04 11:15:09 +0000

    init preprocess

--- kaggle_preprocess.py
+++ kaggle_preprocess.py
@@ -5,36 +5,42 @@ from sklearn import preprocessing
 import scipy.sparse as sp
 import fileinput
 import numpy
-
-
+from itertools import izip
+    
 def imputer():
+    target = []
     train = []
+
     for iline in fileinput.input():
         iline = iline.strip().split(',')
-        try:
-            train.append([float(x) if x else numpy.NaN for x in iline])
-        except:
-            pass
+        t = int(iline[0])
+        v = [(float(x) if x else numpy.NaN) for x in iline[1:]]
+	
+        target.append(t)
+        train.append(v)
 
     imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 #    print train
 #    train = sp.csc_matrix(train)
-    imp.fit(train[:10000])
-    results = []
-    while train:
-        results.extend(imp.transform(train[:10000]))
-        del train[:10000]
+    imp.fit(train)
+    train = imp.transform(train)
+    #results = []
+    #while train:
+    #    results.extend(imp.transform(train[:10000]))
+    #    del train[:10000]
 
+    #train = results
     #train = [imp.transform(train[k:k+10000]) for k in xrange(0, len(train), 10000)] 
     #train = imp.transform(train)
-#    train = preprocessing.scale(results)
+    train = preprocessing.scale(train)
 
 #    h, w =  train.shape
 
 #    for index in xrange(h):
 #        iline = train.getrow(index).toarray()[0]
-    for iline in train:
-        print ",".join(map(str, iline))
+    for itarget, itrain in izip(target, train):
+        print itarget, ',',
+        print ",".join(map(str, itrain))
 
 if __name__ == "__main__":
     imputer()
