1b6d0fd1f5e2ba66316b9b4ef07f07abb1803151
lucemia <lucemia@gmail.com>
2014-08-05 17:38:12 +0000

    got 0.54332

--- kaggle_sample.py
+++ kaggle_sample.py
@@ -4,9 +4,10 @@ import fileinput
 import random
 
 def sample():
+    ratio = 0.01
     with open('train.samples.csv', 'w') as train, open('test.samples.csv', 'w') as test:
         for iline in fileinput.input():
-            if random.random() < 0.1:
+            if random.random() < ratio:
                 test.write(iline)
             else:
                 train.write(iline)
--- kaggle_train.py
+++ kaggle_train.py
@@ -4,6 +4,7 @@ import fileinput
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score
 from sklearn.externals import joblib
+from itertools import izip
 
 def io():
     target = []
@@ -19,26 +20,17 @@ def io():
 
     return target, train 
 
-from itertools import izip
-
 def train():
     target, train = io()
 
-    clf = RandomForestClassifier(n_jobs=12, n_estimators=10)
+    clf = RandomForestClassifier(n_jobs=-1, n_estimators=100)
     clf.fit(train, target)
     p_target = clf.predict_proba(train)
-    #scores = cross_val_score(clf, train, target)
 
     for a, b in izip(target, p_target):
         print ','.join(map(str, [a, b[1]]))
      
-#    print scores
-#    print scores.mean()
-
-    #clf.fit(train, target)
-#    return clf    
     joblib.dump(clf, 'train.pkl', compress=9)
 
 if __name__ == "__main__":
-#    import clime.now
     train()
