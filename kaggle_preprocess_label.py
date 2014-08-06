208eac993447c9399dbb551661764d0fcd320c90
lucemia <lucemia@gmail.com>
2014-08-06 18:02:26 +0800

    fin label

--- kaggle_preprocess_label.py
+++ kaggle_preprocess_label.py
@@ -3,14 +3,25 @@
 import fileinput
 from sklearn import preprocessing
 from sklearn.externals import joblib
+from collections import Counter
+
 
 def label():
+    threshold = 1000
     vs = []
+    c = Counter()
+
     for iline in fileinput.input():
         iline = iline.strip().split(',')
+        c.update(iline)
+
         vs.append(iline)
 
-    le = preprocessing.LabelEncoder()
-    le.fit_transform(vs)
+    c = [(label, index) for (label, freq), index in enumerate(c.most_common()) if freq > 1000]
 
-    
+    # Label
+    for iline in vs:
+        print ",".join(map(lambda k: c.get(k,""), iline))
+
+if __name__ == "__main__":
+    label()
