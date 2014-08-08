c62f5ce116d303c0dd256c5ee785209ce703f9f2
lucemia <lucemia@gmail.com>
2014-08-09 01:55:04 +0800

    update process, train

--- kaggle_preprocess.py
+++ kaggle_preprocess.py
@@ -9,6 +9,7 @@ import numpy
 from itertools import izip
 from dpipe import dio
 
+
 def imputer():
     vs = []
     for iline in dio.io():
@@ -20,6 +21,38 @@ def imputer():
 
     for v in vs:
         print ",".join(map(str, map(int, v)))
+
+
+def label(threshold=0.1):
+    vs = []
+    c = Counter()
+
+    for iline in fileinput.input():
+        iline = iline.strip().split(',')
+        c.update(iline)
+
+        vs.append(iline)
+
+    c.pop('')
+    if isinstance(threshold, int):
+        threshold = threshold
+    elif isinstance(threshold, float):
+        threshold = int(len(c) * threshold)
+    else:
+        raise
+
+    c = {label: index for index, (label, freq) in enumerate(c.most_common()[:threshold])}
+
+    vs = [[c[i] for i in k if i in c] for k in vs]
+
+    for v in vs:
+        print ','.join(map(str, v))
+
+    with open('label.model', 'w') as ofile:
+        for label in c:
+            ofile.write('%s,%s\n'%(label, c[label]))
+
+
 
 if __name__ == "__main__":
     dio.now('imputer')
--- kaggle_train.py
+++ kaggle_train.py
@@ -8,6 +8,32 @@ from itertools import izip
 import os, sys
 import dio
 
+def sparse_io():
+    row = []
+    col = []
+    data = []
+    target = []
+
+    for ri, iline in enumerate(dio.io()):
+        iline = iline.strip().split(',')
+        t = int(iline[0])
+        irow = map(int, iline[1:])
+
+        target.append(t)
+
+        for ci, v in enumerate(irow[:13]):
+            row.append(ri)
+            col.append(ci)
+            data.append(v)
+
+        for l in irow[13:]:
+            row.append(ri)
+            col.append(l+13)
+            data.append(1)
+
+    return csr_matrix((data, (row, col)), sharp=(ri, max(col)))
+
+
 def io():
     target = []
     train = []
@@ -15,15 +41,16 @@ def io():
     for iline in dio.io():
         iline = iline.strip().split(',')
         t = int(iline[0])
-        v = map(float, iline[1:])
+        v = map(int, iline[1:])
 
         target.append(t)
         train.append(v)
 
-    return target, train 
+    return target, train
+
 
 def train(n_estimators = 100, **kwargs):
-    target, train = io()
+    target, train = sparse_io()
 
     clf = RandomForestRegressor(n_jobs=-1, n_estimators=n_estimators, **kwargs)
     clf.fit(train, target)
@@ -31,8 +58,7 @@ def train(n_estimators = 100, **kwargs):
 
     for a, b in izip(target, p_target):
         print a, ',', b
-        #print ','.join(map(str, [a, b[1]]))
-     
+
     joblib.dump(clf, 'train.pkl')
 
 if __name__ == "__main__":
