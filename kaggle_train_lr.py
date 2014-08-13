c5d9bbefddbe1e9c757ad9cbba022c5c58e22c16
lucemia <lucemia@gmail.com>
2014-08-13 17:46:48 +0000

    add model load/save support

--- kaggle_train_lr.py
+++ kaggle_train_lr.py
@@ -30,6 +30,7 @@ def io():
         int_fs = map(lambda i: numpy.NaN if not i else int(i), iline[1:14])
         label_fs = [k for k in iline[14:]]
         #label_fs = ",".join(iline[14:])
+#        print int_fs, label_fs
 
         target.append(t)
         train_int.append(int_fs)
@@ -50,7 +51,7 @@ def io():
 #    print train
     return target, train
 
-def train():
+def train(model):
     target, train = io()
 
     lr = LogisticRegression()
@@ -59,6 +60,22 @@ def train():
 
     for a, i in izip(target, p):
         print a, ',', i[1]
+
+    joblib.dump(lr, model)
+
+
+def predict(model):
+    clf = joblib.load(model)
+    ids, tests = io()
+
+    while tests:
+        results = clf.predict_proba(tests[:10000])
+
+        for a, b in izip(ids, results):
+            print a, ',', b
+
+        del ids[:10000]
+        del tests[:10000]
 
 if __name__ == "__main__":
     dio.now()
