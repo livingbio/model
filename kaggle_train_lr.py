c3367ad0cced9e979d4744567cebb630555ef9d3
lucemia <lucemia@gmail.com>
2014-08-13 17:43:27 +0000

    fix bug for lr trainner"

--- kaggle_train_lr.py
+++ kaggle_train_lr.py
@@ -7,15 +7,18 @@ from itertools import izip
 import os, sys
 from dpipe import dio
 from sklearn.metrics import fbeta_score, make_scorer
-from sklearn.feature_extraciton.text import HashingVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction import FeatureHasher
 from kaggle_evl import *
 import random
 from scipy.sparse import csr_matrix, hstack
 from sklearn.linear_model import LogisticRegression
-
+import numpy
+from sklearn.preprocessing import Imputer
+from sklearn import preprocessing
 
 def io():
-    hv = HashingVectorizer()
+    hv = FeatureHasher()
 
     target = []
     train_int = []
@@ -24,17 +27,27 @@ def io():
     for iline in dio.io():
         iline = iline.strip().split(',')
         t = int(iline[0])
-        int_fs = map(int, iline[1:14])
-        label_fs = iline[14:]
+        int_fs = map(lambda i: numpy.NaN if not i else int(i), iline[1:14])
+        label_fs = [k for k in iline[14:]]
+        #label_fs = ",".join(iline[14:])
 
         target.append(t)
         train_int.append(int_fs)
-        train_label.append(label_fs)
+        train_label.append({k:1 for k in label_fs})
 
+#    print train_int
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    train_int = imp.fit_transform(train_int)
+#    print train_int
+    scaler = preprocessing.StandardScaler().fit(train_int)
+    train_int = scaler.transform(train_int)
+#    print train_int
     train_int = csr_matrix(train_int)
+#    print train_label
     train_label = hv.transform(train_label)
-    train = hstack(train_int, train_label)
-
+    train = hstack((train_int, train_label))
+#    print train_label
+#    print train
     return target, train
 
 def train():
@@ -44,8 +57,8 @@ def train():
     lr.fit(train, target)
     p = lr.predict_proba(train)
 
-    for i in p:
-        print i
+    for a, i in izip(target, p):
+        print a, ',', i[1]
 
 if __name__ == "__main__":
     dio.now()
